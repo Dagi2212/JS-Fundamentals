a4436ee7a34f53e6f1ec09af17139f3f2bbfeb38
Dagi2212 <167195227+Dagi2212@users.noreply.github.com>
2025-05-25 13:47:33 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,7 @@
-const langs=['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for(int i=0; i< langs.length;i++){
+
+
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
